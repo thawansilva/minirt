37ad8121cd271a1bf5f7b3e7fee43e45cc929741
Thawan Silva <twnpsilva@gmail.com>
2026-03-24 21:02:44 -0300

    cleanup: Remove unused variables

--- config.mk
+++ config.mk
@@ -4,13 +4,9 @@ CFLAGS = -Wall -Wextra -Werror
 
 SRC_DIR = src
 INC_DIR = inc
-LIBFT_DIR = libft
-LIBFT = $(LIBFT_DIR)/libft.a
 
 INCLUDES = -I./$(INC_DIR) -I$(LIBFT_DIR)/inc
 
-EVAL_DIR = eval
-HASHTABLE_DIR = hashtable
 SRC_FILES = main.c \
 
 BONUS_DIR = bonus
