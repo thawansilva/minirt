23881cd65853155b3715734a869ec204c4d89f31
Thawan Silva <twnpsilva@gmail.com>
2026-04-04 11:16:00 -0300

    wip: Implement read_file function

--- src/validation/input.c
+++ src/validation/input.c
@@ -1,5 +1,10 @@
 #include "libft.h"
 #include "validation.h"
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
 
 static void	print_error(char *msg, int exit_code)
 {
@@ -22,15 +27,29 @@ int	is_valid_extension(char *file)
 
 void	read_file(char *file, t_scene *scene)
 {
-	scene->lines = ft_split_charset(file, " \t");
-	if (!scene->lines)
-		print_error("Split info scenes", 1);
-	
+	int		fd;
+	char	*line;
+
+	if (!file)
+		print_error("Missing file", 2);
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+	{
+		perror(strerror(errno));
+		exit(1);
+	}
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		ft_lstadd_back(&(scene->objs), ft_lstnew(line));
+		line = get_next_line(fd);
+	}
+	close(fd);
 }
 
 void	validate_input(char *file, t_scene *scene)
 {
 	if (!is_valid_extension(file))
 		print_error("Invalid extension", 1);
-//	read_file(char *file, t_scene *scene);
+	read_file(file, scene);
 }
