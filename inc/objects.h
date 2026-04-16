26202c37a758dc6d8423154a1c5111776455ef8d
herom-s <heromapp1@gmail.com>
2026-04-16 15:41:22 -0300

    refator(math): changing name of struct field in s_surface_parameters from k to diameter

--- inc/objects.h
+++ inc/objects.h
@@ -6,7 +6,7 @@
 /*   By: hermarti <hermarti@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 09:18:26 by hermarti          #+#    #+#             */
-/*   Updated: 2026/04/15 17:13:25 by hermarti         ###   ########.fr       */
+/*   Updated: 2026/04/16 15:37:35 by hermarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,7 @@ typedef struct s_surface_parameters
 	t_vec4			orientation;
 	t_vec4			w;
 	t_vec4			l;
-	double			k;
+	double			diameter;
 	double			height;
 	t_surface_type	type;
 }					t_surface_parameters;
